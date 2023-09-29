11e4c975ec71190f346aefdc084a83ca386078c2
Youness0f <146443299+Youness0f@users.noreply.github.com>
2023-09-29 16:46:38 +0100

    Create video.js

---
+++ store/video.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+  // Your store module state
+});
+
+export const mutations = {
+  // Your store module mutations
+};
+
+export const actions = {
+  // Your store module actions
+};
