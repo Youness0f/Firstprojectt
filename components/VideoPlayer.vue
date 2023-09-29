ae0d3b36ed9e650441017ff8db1f38f969e61126
Youness0f <146443299+Youness0f@users.noreply.github.com>
2023-09-29 16:42:31 +0100

    Create VideoPlayer.vue

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- Your video player component content -->
+    <video controls>
+      <source src="video.mp4" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  // Your component logic here
+}
+</script>
