9646c16b59fd9bd74edaa27158c19d034358cfb7
Youness0f <146443299+Youness0f@users.noreply.github.com>
2023-09-29 16:44:26 +0100

    Create index.vue

---
+++ layouts/middleware/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- Your main page content -->
+    <h1>Welcome to Live Video Clip Cutter</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // Your page logic here
+}
+</script>
